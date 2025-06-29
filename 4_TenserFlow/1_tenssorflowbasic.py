93351620f6c6221bda90a5f68cb28ce8ca3f1010
Narayan <nanacharya123@gmail.com>
2025-06-29 13:57:03 -0400

    added tensorflow

---
+++ 4_TenserFlow/1_tenssorflowbasic.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+print("test")
+print(tf.__version__)
+print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+print("Physical devices:", tf.config.list_physical_devices())
---
+++ 4_TenserFlow/Readme.md
@@ -0,0 +1,4 @@
+brew install python@3.11
+python3.11 -m venv tf-env
+source tf-env/bin/activate
+pip install tensorflow
